19ab85a46a694a8de87a78035800fd0db72bde72
HamadMulti <50540486+HamadMulti@users.noreply.github.com>
2024-11-15 07:36:41 +0300

    Update urls.py

--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -11,4 +11,10 @@ urlpatterns = [
     path('ecommerce/', include('ecommerce.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='ecommerce/login.html'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(template_name='ecommerce/logout.html'), name='logout'),
+
+    # Password Reset Views
+    path('password_reset/', auth_views.PasswordResetView.as_view(template_name='ecommerce/password_reset_form.html'), name='password_reset'),
+    path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(template_name='ecommerce/password_reset_done.html'), name='password_reset_done'),
+    path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='ecommerce/password_reset_confirm.html'), name='password_reset_confirm'),
+    path('reset/done/', auth_views.PasswordResetCompleteView.as_view(template_name='ecommerce/password_reset_complete.html'), name='password_reset_complete'),
 ]
