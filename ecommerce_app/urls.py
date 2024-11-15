681ccd7d7044dd5deef3ad38f63b99dd98ac035b
HamadMulti <50540486+HamadMulti@users.noreply.github.com>
2024-11-15 09:10:43 +0300

    Update urls.py

--- ecommerce_app/urls.py
+++ ecommerce_app/urls.py
@@ -1,5 +1,3 @@
-# ecommerce_app/urls.py
-
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
@@ -10,7 +8,9 @@ urlpatterns = [
     path('', views.product_list, name='home'),  # Root URL points to product_list view
     path('ecommerce/', include('ecommerce.urls')),
     path('login/', auth_views.LoginView.as_view(template_name='ecommerce/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(template_name='ecommerce/logout.html'), name='logout'),
+    
+    # Updated Logout URL without custom template
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),  # No need for custom template here
 
     # Password Reset Views
     path('password_reset/', auth_views.PasswordResetView.as_view(template_name='ecommerce/password_reset_form.html'), name='password_reset'),
